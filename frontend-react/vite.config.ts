7b0ec9b05b496f46d64f1e3dec79735991f3dbcc
mohammad <paniapandora@gmail.com>
2026-09-10 07:46:12 -0700

    Add files via upload

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/',
     build: {
         outDir: process.env.APPDEPLOY_VITE_OUT_DIR || 'dist',
         sourcemap: process.env.APPDEPLOY_VITE_SOURCEMAP === 'hidden' ? 'hidden' : false,
