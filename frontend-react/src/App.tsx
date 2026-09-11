b4daf4853d33c9fc53a4129521fdbd705c044db6
mohammad <paniapandora@gmail.com>
2026-09-11 07:58:18 -0700

    feat: mount personalized onboarding

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import DiscoveryApp from './app/DiscoveryApp';
 import AIChat from './components/AIChat';
+import OnboardingGate from './components/OnboardingGate';
 import { recordIncomingReferralFromLocation } from './data/growth';
 
 function App() {
@@ -8,10 +9,15 @@ function App() {
         void recordIncomingReferralFromLocation().catch(() => {});
     }, []);
 
+    const refreshPersonalizedFeed = () => {
+        window.setTimeout(() => window.location.reload(), 120);
+    };
+
     return (
         <>
             <DiscoveryApp />
             <AIChat />
+            <OnboardingGate onComplete={refreshPersonalizedFeed} />
         </>
     );
 }
