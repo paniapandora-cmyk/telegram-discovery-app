63664d970382323dfd0797f9ca06ffcee536b653
mohammad <paniapandora@gmail.com>
2026-09-11 06:50:05 -0700

    feat: record incoming referral opens

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import DiscoveryApp from './app/DiscoveryApp';
 import AIChat from './components/AIChat';
+import { recordIncomingReferralFromLocation } from './data/growth';
 
 function App() {
+    useEffect(() => {
+        void recordIncomingReferralFromLocation().catch(() => {});
+    }, []);
+
     return (
         <>
             <DiscoveryApp />
