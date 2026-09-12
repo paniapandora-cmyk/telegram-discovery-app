f9db0c259e5a8ee47719270a6cfaf4710471d195
mohammad <paniapandora@gmail.com>
2026-09-12 15:19:11 -0700

    feat: add personalization route

--- frontend-react/src/types.ts
+++ frontend-react/src/types.ts
@@ -1,4 +1,4 @@
-export type Page='home'|'explore'|'search'|'saved'|'profile'|'history'|'creator'|'notifications'|'invite'|'support'|'ads';
+export type Page='home'|'explore'|'search'|'saved'|'profile'|'history'|'creator'|'notifications'|'invite'|'support'|'ads'|'personalization';
 export type Channel={
   id:string;
   title:string;
